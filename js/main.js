699029cfb50d8cb6fcbc3c5dd4342e29ea58858e
Lisa Long <garrisonridge@gmail.com>
2017-03-28 21:56:17 -0400

    Add and style scrollUp jQuery plugin

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
 
+//mmenu
     $('#my-menu').mmenu({
     // options
     }, {
@@ -15,4 +16,15 @@ $(document).ready(function () {
         API.open();
     });
 
+//scrollup - default elements listed - I may change them
+    $.scrollUp({
+        scrollName: 'scrollUp', // Element ID
+        topDistance: '300', // Distance from top before showing element (px)
+        topSpeed: 300, // Speed back to top (ms)
+        animation: 'fade', // Fade, slide, none
+        animationInSpeed: 200, // Animation in speed (ms)
+        animationOutSpeed: 200, // Animation out speed (ms)
+        scrollText: 'Scroll to top', // Text for element
+        activeOverlay: false, // Set CSS color to display scrollUp active point, e.g '#00FFFF'
+    });
 });
